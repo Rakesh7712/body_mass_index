1790e98725e21165e8ececa3e41026d9adb61b51
Rakesh Burla <101574250+Rakesh7712@users.noreply.github.com>
2023-11-03 12:29:02 +0530

    Add files via upload

---
+++ bmi.py
@@ -0,0 +1,105 @@
+from tkinter import *
+from tkinter import messagebox
+
+def Bmi():
+    age_tf.delete(0,'end')
+    height_tf.delete(0,'end')
+    weight_tf.delete(0,'end')
+
+def calculate_bmi():
+    kg = int(weight_tf.get())
+    m = int(height_tf.get())/100
+    bmi = kg/(m*m)
+    bmi = round(bmi, 1)
+    bmi_index(bmi)
+
+def bmi_index(bmi):
+    
+    if bmi < 18.5:
+        messagebox.showinfo('bmi-pythonguides', f'BMI = {bmi} is Underweight')
+    elif (bmi > 18.5) and (bmi < 24.9):
+        messagebox.showinfo('bmi-pythonguides', f'BMI = {bmi} is Normal')
+    elif (bmi > 24.9) and (bmi < 29.9):
+        messagebox.showinfo('bmi-pythonguides', f'BMI = {bmi} is Overweight')
+    elif (bmi > 29.9):
+        messagebox.showinfo('bmi-pythonguides', f'BMI = {bmi} is Obesity') 
+    else:
+        messagebox.showerror('bmi-pythonguides', 'something went wrong!')   
+
+ws = Tk()
+ws.title('PythonGuides')
+ws.geometry('400x300')
+ws.config(bg='#686e70')
+
+var = IntVar()
+
+frame = Frame(
+    ws,
+    padx=10, 
+    pady=10
+)
+frame.pack(expand=True)
+
+
+age_lb = Label(
+    frame,
+    text="Enter Your Age:"
+)
+age_lb.grid(row=1, column=1)
+
+age_tf = Entry(
+    frame, 
+)
+age_tf.grid(row=1, column=2, pady=5)
+
+
+height = Label(
+    frame,
+    text="Enter your Height in cm  "
+)
+height.grid(row=3, column=1)
+
+weight = Label(
+    frame,
+    text="Enter your Weight in kg  ",
+
+)
+weight.grid(row=4, column=1)
+
+height_tf = Entry(
+    frame,
+)
+height_tf.grid(row=3, column=2, pady=5)
+
+weight_tf = Entry(
+    frame,
+)
+weight_tf.grid(row=4, column=2, pady=5)
+
+frame3 = Frame(
+    frame
+)
+frame3.grid(row=5, columnspan=3, pady=10)
+
+cal_btn = Button(
+    frame3,
+    text='Calculate',
+    command=calculate_bmi
+)
+cal_btn.pack(side=LEFT)
+
+reset_btn = Button(
+    frame3,
+    text='Reset',
+    command=Bmi
+)
+reset_btn.pack(side=LEFT)
+
+exit_btn = Button(
+    frame3,
+    text='Exit',
+    command=lambda:ws.destroy()
+)
+exit_btn.pack(side=RIGHT)
+
+ws.mainloop()
